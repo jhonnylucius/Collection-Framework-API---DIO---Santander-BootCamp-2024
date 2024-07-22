ea1e61301e0c55bc3d92401cf67a2dd3d08f8d37
jhonnylucius <lucianofloripa@outlook.com>
2024-07-22 18:11:30 -0300

    Desafio Ordenação de lista

---
+++ ListaDeTarefas/src/ordenacaoPessoas/OrdenacaoPessoa.java
@@ -0,0 +1,18 @@
+package ordenacaoPessoas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    //atributos
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        
+    }
+
+}
