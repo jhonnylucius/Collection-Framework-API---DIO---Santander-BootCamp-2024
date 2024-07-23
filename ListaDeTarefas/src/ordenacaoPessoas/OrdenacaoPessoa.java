998d6a46886c93db7a79f428184524f6d60aa547
jhonnylucius <lucianofloripa@outlook.com>
2024-07-23 15:23:23 -0300

    finalizado desafio de ordenação com compareble e comparetor

--- ListaDeTarefas/src/ordenacaoPessoas/OrdenacaoPessoa.java
+++ ListaDeTarefas/src/ordenacaoPessoas/OrdenacaoPessoa.java
@@ -1,18 +1,63 @@
 package ordenacaoPessoas;
 
+
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import ordenacaoPessoas.Pessoa.ComparetorPorAltura;
+
 public class OrdenacaoPessoa {
-    //atributos
-    private List<Pessoa> pessoaList;
+  //atributo
+  private List<Pessoa> pessoaList;
 
-    public OrdenacaoPessoa() {
-        this.pessoaList = new ArrayList<>();
+  public OrdenacaoPessoa() {
+    this.pessoaList = new ArrayList<>();
+  }
+
+  public void adicionarPessoa(String nome, int idade, double altura) {
+    pessoaList.add(new Pessoa(nome, idade, altura));
+  }
+
+  public List<Pessoa> ordenarPorIdade() {
+    List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+    if (!pessoaList.isEmpty()) {
+      Collections.sort(pessoasPorIdade);
+      return pessoasPorIdade;
+    } else {
+      throw new RuntimeException("A lista está vazia!");
     }
+  }
 
-    public void adicionarPessoa(String nome, int idade, double altura) {
-        
+  public List<Pessoa> ordenarPorAltura() {
+    List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+    if (!pessoaList.isEmpty()) {
+      Pessoa pessoa = new Pessoa(null, 0, 0);      
+      Collections.sort(pessoasPorAltura, pessoa.new ComparetorPorAltura());
+      return pessoasPorAltura;
     }
+    return pessoasPorAltura; 
+    
+  }
 
+  public static void main(String[] args) {
+  // Criando uma instância da classe OrdenacaoPessoas
+    OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+
+    // Adicionando pessoas à lista
+    ordenacaoPessoa.adicionarPessoa("Alice", 20, 1.56);
+    ordenacaoPessoa.adicionarPessoa("Bob", 30, 1.80);
+    ordenacaoPessoa.adicionarPessoa("Charlie", 25, 1.70);
+    ordenacaoPessoa.adicionarPessoa("David", 17, 1.56);
+
+    // Exibindo a lista de pessoas adicionadas
+    System.out.println(ordenacaoPessoa.pessoaList);
+
+    // Ordenando e exibindo por idade
+    System.out.println(ordenacaoPessoa.ordenarPorIdade());
+
+    // Ordenando e exibindo por altura
+    System.out.println(ordenacaoPessoa.ordenarPorAltura());
+  }
 }
