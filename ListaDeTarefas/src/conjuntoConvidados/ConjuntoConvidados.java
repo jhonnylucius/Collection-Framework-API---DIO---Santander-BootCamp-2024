efbf3f4e7c06301fad174dd61f62cbfb94da49aa
jhonnylucius <lucianofloripa@outlook.com>
2024-07-24 10:07:34 -0300

    Finalizado testes e desafio conjuntoConvidados

--- ListaDeTarefas/src/conjuntoConvidados/ConjuntoConvidados.java
+++ ListaDeTarefas/src/conjuntoConvidados/ConjuntoConvidados.java
@@ -8,7 +8,7 @@ public class ConjuntoConvidados {
     private static Set<Convidados> convidadosSet;
 
     public ConjuntoConvidados(Set<Convidados> convidadosSet) {
-        this.convidadosSet = new HashSet<>();
+        ConjuntoConvidados.convidadosSet = new HashSet<>();
     }
 
     public void adicionarConvidados(String nome, int numeroConvite ) {
@@ -37,15 +37,18 @@ public class ConjuntoConvidados {
 public static void main(String[] args) {
     ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados(convidadosSet);
     System.out.println("Existem " + ConjuntoConvidados.contarConvidados() + " dentro do set de convidados");
-
+// add o convidados
     conjuntoConvidados.adicionarConvidados("Lenísia", 0123);
     conjuntoConvidados.adicionarConvidados("Lucas", 0124);
     conjuntoConvidados.adicionarConvidados("Laura", 0125);
     conjuntoConvidados.adicionarConvidados("Luciano", 0123);
-
+// exibe a lista de convidados, exibindo os convidados acima, só que um convite é clonado
+// o set não aceita convite com msm codigo pela classe convidados usando
+// equals and hashCode
     conjuntoConvidados.exibirConvidados();
         System.out.println("Existem " + ConjuntoConvidados.contarConvidados() + " dentro do set de convidados");
-
+// removendo um convidado, note que ele remove a Lenísia, 
+//e não o Luciano pois ele nem foi gravado, não sendo valido o sistema remove o codigo valido 
     conjuntoConvidados.removerConvidadoPorNumeroConvite(0123);
         System.out.println("Existem " + ConjuntoConvidados.contarConvidados() + " dentro do set de convidados");
 
