584f255eb6c1e980c56194013530a36bbc4119e6
jhonnylucius <lucianofloripa@outlook.com>
2024-07-25 17:19:47 -0300

    Finalizado exercício com Map com uma agenda de contatos simples e add um readme

---
+++ ListaDeTarefas/src/exerciciosMap/agendaContatos/AgendaContatos.java
@@ -0,0 +1,62 @@
+package exerciciosMap.agendaContatos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+
+    // atributos
+    private Map<String, Integer> agendaContatosMap;
+
+    // pela IDE crie os construtores
+    public AgendaContatos() {
+        this.agendaContatosMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone) {
+        agendaContatosMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome) {
+        if (!agendaContatosMap.isEmpty()) {
+            agendaContatosMap.remove(nome);
+        }
+    }
+
+    // Exibe os contatos de forma organizada
+    public void exibirContatos() {
+        if (!agendaContatosMap.isEmpty()) {
+            for (Map.Entry<String, Integer> entry : agendaContatosMap.entrySet()) {
+                System.out.println("Nome: " + entry.getKey() + ", Telefone: " + entry.getValue());
+            }
+        } else {
+            System.out.println("A agenda está vazia.");
+        }
+    }
+
+    // Corrigido para procurar o número se a agenda não estiver vazia
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome = null;
+        if (!agendaContatosMap.isEmpty()) {
+            numeroPorNome = agendaContatosMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+        agendaContatos.adicionarContato("lucas", 367985467);
+        agendaContatos.adicionarContato("laura", 323165455);
+        agendaContatos.adicionarContato("luciano", 349245223);
+
+        agendaContatos.exibirContatos(); // Exibe os contatos
+
+        // Exemplo de pesquisa
+        Integer telefoneLucas = agendaContatos.pesquisarPorNome("lucas");
+        if (telefoneLucas != null) {
+            System.out.println("O telefone de Lucas é: " + telefoneLucas);
+        } else {
+            System.out.println("Lucas não está na agenda.");
+        }
+    }
+}
